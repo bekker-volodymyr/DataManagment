16745896eca274276f9d220fb64d9f9ba8a8053b
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-05-20 12:56:13 +0300

    Intro Lesson

---
+++ EFCoreIntro/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EFCoreIntro.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreIntro
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EfDemoDb;");
+        }
+    }
+}
---
+++ EFCoreIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using EFCoreIntro.Entities;
+
+namespace EFCoreIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Ініціалізація контексту
+            AppDbContext db = new AppDbContext();
+
+            // Створення об'єктів сутностей
+            Student student = new Student() { FirstName = "Антон", LastName = "Степанюк", Age = 17 };
+
+            // Додавання до БД
+            db.Students.Add(student);
+
+            // Збереження доданих записів
+            db.SaveChanges();
+
+            // Читання даних
+            var students = db.Students.ToList();
+            foreach (var s in students)
+            {
+                Console.WriteLine($"Ім'я: {s.LastName} {s.FirstName}\nВік: {s.Age}");
+            }
+        }
+    }
+}
